4fef759ba4508bfd03ff6bbc8955003d554f2df2
David Dammerer <david.dammerer08@htl-salzburg.ac.at>
2026-05-05 14:39:43 +0200

    made the lightswitch not fully functional

--- vr/UnityProject/shsi-vr/Assets/MyApp/Scripts/VRButton.cs
+++ vr/UnityProject/shsi-vr/Assets/MyApp/Scripts/VRButton.cs
@@ -1,8 +1,43 @@
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
+[RequireComponent(typeof(XRSimpleInteractable))]
 public class VRButton : MonoBehaviour
 {
+    XRSimpleInteractable m_Interactable;
+
+    void Awake()
+    {
+        m_Interactable = GetComponent<XRSimpleInteractable>();
+
+        if (m_Interactable == null)
+        {
+            Debug.LogError($"{nameof(VRButton)} requires an {nameof(XRSimpleInteractable)} on the same GameObject.", this);
+        }
+    }
+
+    void OnEnable()
+    {
+        if (m_Interactable == null)
+            return;
+
+        m_Interactable.selectEntered.AddListener(OnSelectEntered);
+    }
+
+    void OnDisable()
+    {
+        if (m_Interactable == null)
+            return;
+
+        m_Interactable.selectEntered.RemoveListener(OnSelectEntered);
+    }
+
+    void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        OnPress();
+    }
+
     public void OnPress()
     {
         Debug.Log("VR Button Pressed!");
