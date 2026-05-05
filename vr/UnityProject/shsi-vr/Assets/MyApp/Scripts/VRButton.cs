e66be2b4b27f7229df8edd7d46833fa2b82cf29c
David Dammerer <david.dammerer08@htl-salzburg.ac.at>
2026-05-05 14:09:41 +0200

    Added Lightswitch for Testing

---
+++ vr/UnityProject/shsi-vr/Assets/MyApp/Scripts/VRButton.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class VRButton : MonoBehaviour
+{
+    public void OnPress()
+    {
+        Debug.Log("VR Button Pressed!");
+    }
+}
