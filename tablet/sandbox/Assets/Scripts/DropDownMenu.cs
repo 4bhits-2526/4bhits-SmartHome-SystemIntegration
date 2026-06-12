3f6b5cd03482179cdfef5c68bcf9f607416fdadc
gwagner <gabriel.wagner08@htl-salzburg.ac.at>
2026-06-12 10:18:54 +0200

    Arrow fixes with Images

--- tablet/sandbox/Assets/Scripts/DropDownMenu.cs
+++ tablet/sandbox/Assets/Scripts/DropDownMenu.cs
@@ -1,11 +1,11 @@
-using TMPro;
 using UnityEngine;
 
 public class RoomDropdownButton : MonoBehaviour
 {
-    public TMP_Text buttonText;
-    public TMP_Text buttonText2;
-    public TMP_Text buttonText3;
+    [Header("Arrow Transforms")]
+    public RectTransform arrow1;
+    public RectTransform arrow2;
+    public RectTransform arrow3;
 
     private bool isOpen1 = false;
     private bool isOpen2 = false;
@@ -14,48 +14,27 @@ public class RoomDropdownButton : MonoBehaviour
     public void ToggleDropdown1()
     {
         isOpen1 = !isOpen1;
-
-        if (isOpen1)
-        {
-            buttonText.text = "\nClick me! ▲";
-
-        }
-        else
-        {
-            buttonText.text = "\nClick me! ▼";
-
-        }
+        RotateArrow(arrow1, isOpen1);
     }
 
     public void ToggleDropdown2()
     {
         isOpen2 = !isOpen2;
-        if (isOpen2)
-        {
-             buttonText2.text = "\nClick me! ▲";
-        }
-        else
-        {
-            buttonText2.text = "\nClick me! ▼";
-        }
+        RotateArrow(arrow2, isOpen2);
     }
 
     public void ToggleDropdown3()
     {
         isOpen3 = !isOpen3;
-        if (isOpen3)
-        {
-            buttonText3.text = "\nClick me! ▲";
-        }
-        else
-        {
-            buttonText3.text = "\nClick me! ▼";
-        }
+        RotateArrow(arrow3, isOpen3);
+    }
+
+    private void RotateArrow(RectTransform arrow, bool isOpen)
+    {
+        if (arrow == null) return;
+
+        // geschlossen = 0°, offen = 180°
+        float zRotation = isOpen ? 180f : 0f;
+        arrow.localRotation = Quaternion.Euler(0f, 0f, zRotation);
     }
 }
-
-           
-            
-
-            
-            
